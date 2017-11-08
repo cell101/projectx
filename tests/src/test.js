8fd1ec22573c8572e8f88a21ad79ccf47c551aad
cell101 <seungchul.g.lee@gmail.com>
2017-11-08 16:25:32 +0900

    add TODO html

--- tests/src/test.js
+++ tests/src/test.js
@@ -1,8 +1,22 @@
 module.exports ={
-    'Site Up': function(browser){
+    'TODO list': function(browser){
         browser
+
+        // visit main page to check if the title is "Project X"
         .url('localhost:8080')
         .assert.title('Project X')
-        .end();
+
+        // click "todo" link to visit todo page
+        // heck if the title is "TODO list"
+        .useXpath()
+        .waitForElementVisible("//a[text()='TODO']",1000)
+        .click("//a[text()='TODO']")
+        .useCss()
+        .assert.title('TODO list');
+
+
+        
+        browser.assert.fail('Finish the test');
+        browser.end();
     }
 }
