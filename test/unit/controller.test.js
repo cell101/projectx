7464b3806e233cd74be1afdd057c5ea6ccbba8c6
cell101 <seungchul.g.lee@gmail.com>
2017-11-13 19:35:00 +0900

    add an item into a todo list

--- test/unit/controller.test.js
+++ test/unit/controller.test.js
@@ -1,7 +1,22 @@
-describe('',function(){
-    it('',function(){
+describe('MainController',function(){
+    beforeEach(module('demo'));
+    it('Should have a todo list',inject(function($controller){
+        var scpe ={};
+        var myController = $controller('MainController',{$scope:scope});
 
-    });
+        myController.list.should.not.empty;
+        myController.list.should.contain('Finish First Push');
+    }));
+
+    it('Should add a new item',inject(function($controller){
+        var scpe ={};
+        var myController = $controller('MainController',{$scope:scope});
+
+        scope.addItem("Read 'No Silver Bullet")
+        myController.list.should.contain("Read 'No Silver Bullet");
+    }));
+
+
 });
 
 describe('',function(){
