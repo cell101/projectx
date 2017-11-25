432d55bee704ea5d28560781f0a5e2fa4166311a
cell101 <seungchul.g.lee@gmail.com>
2017-11-25 10:42:41 +0900

    newrelic install

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Teampx'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '35a3f2f7188c780eacd0e0920b98d23473e9dcc0',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
